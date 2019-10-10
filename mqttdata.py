f4afad0c17f1bc794cff4a0ea738437d547c53e2
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-10 19:07:29 +0530

    values fixed what to be displayed on dustbin interface

--- mqttdata.py
+++ mqttdata.py
@@ -41,7 +41,46 @@ def on_message(mosq, obj, msg):
     # dustbin1 data posting
     if msg.topic == "container1DataGreen":
         fp = open("C:\Apache24\htdocs\Data\Dustbin_1_Green.json", "w")
-        print(payload[2:(len(payload) - 1)], file=fp)
+        if 76 < (int(payload[2:(len(payload) - 1)])):
+            print("0", file=fp)
+        elif 72 < (int(payload[2:(len(payload) - 1)])) <= 76:
+            print("5", file=fp)
+        elif 68 < (int(payload[2:(len(payload) - 1)])) <= 72:
+            print("10", file=fp)
+        elif 64 < (int(payload[2:(len(payload) - 1)])) <= 68:
+            print("15", file=fp)
+        elif 60 < (int(payload[2:(len(payload) - 1)])) <= 64:
+            print("20", file=fp)
+        elif 56 < (int(payload[2:(len(payload) - 1)])) <= 60:
+            print("25", file=fp)
+        elif 52 < (int(payload[2:(len(payload) - 1)])) <= 56:
+            print("30", file=fp)
+        elif 48 < (int(payload[2:(len(payload) - 1)])) <= 52:
+            print("35", file=fp)
+        elif 44 < (int(payload[2:(len(payload) - 1)])) <= 48:
+            print("40", file=fp)
+        elif 40 < (int(payload[2:(len(payload) - 1)])) <= 44:
+            print("45", file=fp)
+        elif 36 < (int(payload[2:(len(payload) - 1)])) <= 40:
+            print("50", file=fp)
+        elif 32 < (int(payload[2:(len(payload) - 1)])) <= 36:
+            print("55", file=fp)
+        elif 28 < (int(payload[2:(len(payload) - 1)])) <= 32:
+            print("60", file=fp)
+        elif 24 < (int(payload[2:(len(payload) - 1)])) <= 28:
+            print("65", file=fp)
+        elif 20 < (int(payload[2:(len(payload) - 1)])) <= 24:
+            print("70", file=fp)
+        elif 16 < (int(payload[2:(len(payload) - 1)])) <= 20:
+            print("75", file=fp)
+        elif 12 < (int(payload[2:(len(payload) - 1)])) <= 16:
+            print("80", file=fp)
+        elif 8 < (int(payload[2:(len(payload) - 1)])) <= 12:
+            print("85", file=fp)
+        elif 4 < (int(payload[2:(len(payload) - 1)])) <= 8:
+            print("90", file=fp)
+        elif 0 < (int(payload[2:(len(payload) - 1)])) <= 4:
+            print("100", file=fp)
         fp.close()
     if msg.topic == "container1DataBlue":
         fp = open("C:\Apache24\htdocs\Data\Dustbin_1_Blue.json", "w")
@@ -98,7 +137,7 @@ if __name__ == '__main__':
     SERIAL_PORT = "/dev/ttyS0"
     ser = serial.Serial(SERIAL_PORT, baudrate=9600, timeout=5)
 
-    data = data.data()
+    data = data.data()  # my credential
     client = paho.Client()
 
     client.on_message = on_message
