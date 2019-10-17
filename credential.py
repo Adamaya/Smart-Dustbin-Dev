cb36920faeb9bdf0bb49b4da3ead29bfb130f7b4
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-17 23:54:18 +0530

    ip changed

--- credential.py
+++ credential.py
@@ -1,2 +1,3 @@
 def data():
-    return {"ip":"192.168.137.145","user1":"dustbin1","user2":"dustbin2","user3":"dustbin3","user4":"dustbin4","user5":"dustbin5","pw":"12345678","port":1883}
+    return {"ip": "192.168.31.63", "user1": "dustbin1", "user2": "dustbin2", "user3": "dustbin3", "user4": "dustbin4",
+            "user5": "dustbin5", "pw": "12345678", "port": 1883}
