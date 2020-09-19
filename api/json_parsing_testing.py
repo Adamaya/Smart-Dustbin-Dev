db4a750f3c5e1e2756badc2a0a15d132c53c5f3b
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-09-19 18:24:11 +0530

    comments added

--- api/json_parsing_testing.py
+++ api/json_parsing_testing.py
@@ -1,6 +1,8 @@
 import requests
 
 # live_dustbin_status api
+# 'https://qdsnh8xlo9.execute-api.ap-south-1.amazonaws.com/default'
+# "http://localhost/cgi-bin/live_dustbin_status.py"
 res = requests.get('https://qdsnh8xlo9.execute-api.ap-south-1.amazonaws.com/default')
 
 response = res.json()
