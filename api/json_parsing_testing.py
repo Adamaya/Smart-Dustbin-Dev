fe0cab71725ad43a6b0b6b6f4147d35b850de004
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2021-01-01 23:14:47 +0530

    deleted old live_dustbin_status.py file

--- api/json_parsing_testing.py
+++ api/json_parsing_testing.py
@@ -3,7 +3,7 @@ import requests
 # live_dustbin_status api
 # 'https://qdsnh8xlo9.execute-api.ap-south-1.amazonaws.com/default'
 # "http://localhost/cgi-bin/live_dustbin_status.py"
-res = requests.get('https://qdsnh8xlo9.execute-api.ap-south-1.amazonaws.com/default')
+res = requests.get('http://localhost/cgi-bin/temp.py')
 
 response = res.json()
-print(response["dustbin_1"])
+print(type(response))
